869ea66ee01678aa538e55e21edc28eea54921ca
elspeth curtis <elspethcurtis@elspeths-mbp.home>
2021-04-01 11:45:58 -0400

    first pass at designing the first two sections

---
+++ main.js
@@ -0,0 +1,13 @@
+$(".product-header").click(function(){
+    $(".product").toggle();
+  });
+
+  $(".company-header").click(function(){
+    $(".company").toggle();
+  });
+
+  $(".connect-header").click(function(){
+    $(".connect").toggle();
+  });
+
+  
