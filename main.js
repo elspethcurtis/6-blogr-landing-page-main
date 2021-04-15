fd5687d9cd57b78525ec202936f94e8ae0fa472d
elspeth curtis <elspethcurtis@elspeths-mbp.home>
2021-04-15 12:53:59 -0400

    tweakd js file and added overflow-y for the mobile menu

--- main.js
+++ main.js
@@ -1,42 +1,3 @@
-$(".product-header").click(function(){
-    $(".product").toggle();
-    $(".product-chevron-down").toggle();
-    $(".product-chevron-up").toggle();
-  });
-
-  $(".company-header").click(function(){
-    $(".company").toggle();
-    $(".company-chevron-down").toggle();
-    $(".company-chevron-up").toggle();
-  });
-
-  $(".connect-header").click(function(){
-    $(".connect").toggle();
-    $(".connect-chevron-down").toggle();
-    $(".connect-chevron-up").toggle();
-  });
-
-  //mobile nav
-
-  $(".mbl-product-header").click(function(){
-    $(".mbl-nav-list-product").slideToggle();
-    $(".mbl-product-chevron-down").toggle();
-    $(".mbl-product-chevron-up").toggle();
-  });
-
-  $(".mbl-company-header").click(function(){
-    $(".mbl-nav-list-company").slideToggle();
-    $(".mbl-company-chevron-down").toggle();
-    $(".mbl-company-chevron-up").toggle();
-  });
-
-  $(".mbl-connect-header").click(function(){
-    $(".mbl-nav-list-connect").slideToggle();
-    $(".mbl-connect-chevron-down").toggle();
-    $(".mbl-connect-chevron-up").toggle();
-  });
-
-
   //hamburger menu
 
   let menu = document.querySelector(".menu");
@@ -69,3 +30,45 @@ function toggleMenu() {
       menuLink.addEventListener("click", toggleMenu);
     }
   )
+  //HAMBURGER MENU navigation toggles
+
+  $(".mbl-product-header").click(function(){
+    $(".mbl-nav-list-product").slideToggle();
+    $(".mbl-product-chevron-down").toggle();
+    $(".mbl-product-chevron-up").toggle();
+  });
+
+  $(".mbl-company-header").click(function(){
+    $(".mbl-nav-list-company").slideToggle();
+    $(".mbl-company-chevron-down").toggle();
+    $(".mbl-company-chevron-up").toggle();
+  });
+
+  $(".mbl-connect-header").click(function(){
+    $(".mbl-nav-list-connect").slideToggle();
+    $(".mbl-connect-chevron-down").toggle();
+    $(".mbl-connect-chevron-up").toggle();
+  });
+
+//DESKTOP MENU navigation toggles
+$(".product-header").click(function(){
+    $(".product").toggle();
+    $(".product-chevron-down").toggle();
+    $(".product-chevron-up").toggle();
+  });
+
+  $(".company-header").click(function(){
+    $(".company").toggle();
+    $(".company-chevron-down").toggle();
+    $(".company-chevron-up").toggle();
+  });
+
+  $(".connect-header").click(function(){
+    $(".connect").toggle();
+    $(".connect-chevron-down").toggle();
+    $(".connect-chevron-up").toggle();
+  });
+
+
+
+
