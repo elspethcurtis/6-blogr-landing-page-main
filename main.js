e1fdb08ec4bbbb36b0db2645a8ced796a7105ecf
elspeth curtis <elspethcurtis@elspeths-mbp.home>
2021-04-15 11:07:43 -0400

    found the specs file for the hover states and mobile menu so made adjustments to mobile and desktop navigation to match the specs

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 $(".product-header").click(function(){
     $(".product").toggle();
+    $(".product-chevron-down").toggle();
+    $(".product-chevron-up").toggle();
   });
 
   $(".company-header").click(function(){
     $(".company").toggle();
+    $(".company-chevron-down").toggle();
+    $(".company-chevron-up").toggle();
   });
 
   $(".connect-header").click(function(){
     $(".connect").toggle();
+    $(".connect-chevron-down").toggle();
+    $(".connect-chevron-up").toggle();
+  });
+
+  //mobile nav
+
+  $(".mbl-product-header").click(function(){
+    $(".mbl-nav-list-product").slideToggle();
+    $(".mbl-product-chevron-down").toggle();
+    $(".mbl-product-chevron-up").toggle();
+  });
+
+  $(".mbl-company-header").click(function(){
+    $(".mbl-nav-list-company").slideToggle();
+    $(".mbl-company-chevron-down").toggle();
+    $(".mbl-company-chevron-up").toggle();
+  });
+
+  $(".mbl-connect-header").click(function(){
+    $(".mbl-nav-list-connect").slideToggle();
+    $(".mbl-connect-chevron-down").toggle();
+    $(".mbl-connect-chevron-up").toggle();
   });
 
 
